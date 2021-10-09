eba01de4f53ddadd0bcc9d0d177eda00ae1e0b17
Zenahr <zenmatica@gmail.com>
2021-10-09 14:25:39 +0200

    simple version

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import psutil
+import flaskwebgui
+
+app = Flask(__name__)
+gui = flaskwebgui.FlaskUI(app)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/api/cpu')
+def api():
+    return str(psutil.cpu_percent(interval=0.1))
+
+if __name__ == '__main__':
+    gui.run()
